317084c32957a219ed6b3f1186d69269e06a8ef4
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-07-09 19:00:27 -0400

    implemented the exercise requirements

--- client/index.js
+++ client/index.js
@@ -5,10 +5,15 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?   
+  const name = process.argv[2] || 'Norman Block';
+  const index = niceList.findIndex(n => n === name);
+  const mt = new MerkleTree(niceList);
+  const proof = mt.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof: proof,
+    name: name
   });
 
   console.log({ gift });
